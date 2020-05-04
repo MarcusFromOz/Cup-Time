01eb6ae4cc8b9e1479fc0734850397c5b936f9f0
MarkTemby <temby644@gmail.com>
2020-05-04 17:30:20 +0930

    Movement nearly complete

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+}
