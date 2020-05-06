2784a0dda5474d82d114cf09ace8b53259bddac4
MarkTemby <temby644@gmail.com>
2020-05-06 17:47:22 +0930

    Lesson 40

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField] float healthPoints = 100f;
+        bool isDead = false;
+
+        public void TakeDamage(float damage)
+        {
+            healthPoints = Mathf.Max(healthPoints - damage, 0);
+            //print(health);
+
+            if (healthPoints == 0 && isDead == false)
+            {
+                GetComponent<Animator>().SetTrigger("die");
+                isDead = true;
+            }
+        }
+    }
+}
