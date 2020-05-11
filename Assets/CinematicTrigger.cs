18f6927d72a6185c8c31b63820ed2300bf8b5502
MarkTemby <temby644@gmail.com>
2020-05-11 16:35:27 +0930

    Lesson 65

---
+++ Assets/CinematicTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        //** variables
+        bool alreadyTriggered = false;
+
+        //** Start and Update
+
+
+        //** Public Methods
+
+
+        //** Private Methods
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!alreadyTriggered && other.gameObject.tag == "Player")
+            {
+                GetComponent<PlayableDirector>().Play();
+                alreadyTriggered = true;
+            }
+        }
+    }
+}
+
