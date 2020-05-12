91d66090af93347ef5fbb62f8fb06ca62a3a444f
MarkTemby <temby644@gmail.com>
2020-05-12 15:02:10 +0930

    Lesson 74

--- Assets/Portal.cs
+++ Assets/Portal.cs
@@ -2,28 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
 
 public class Portal : MonoBehaviour
 {
 
     //** variables
+
+    enum DestinationIdentifier { A, B, C, D, E }
+
     [SerializeField] int sceneToLoad = -1;
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] DestinationIdentifier destination;
 
     //** Start and Update
-    void Start()
-    {
-       
-    }
-        
+
     //** Public methods
-       
+
     //** Private methods
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
             {
-            SceneManager.LoadScene(1);
+            StartCoroutine(Transition());
         }
     }
+
+    private IEnumerator Transition()
+    {
+        if (sceneToLoad < 0)
+        {
+            Debug.LogError("Scene to load is not set.");
+            yield break;
+        }
+
+        DontDestroyOnLoad(gameObject);
+        yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+        Portal otherPortal = GetOtherPortal();
+        UpdatePlayer(otherPortal);
+
+        Destroy(gameObject);
+    }
+
+    private void UpdatePlayer(Portal otherPortal)
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+
+        //can clash with NavMesh agent
+        //player.transform.position = otherPortal.spawnPoint.position;
+        player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+        player.transform.rotation = otherPortal.spawnPoint.rotation;
+    }
+
+    private Portal GetOtherPortal()
+    {
+        foreach(Portal portal in FindObjectsOfType<Portal>())
+        {
+            if (portal == this) continue;
+            if (portal.destination != destination) continue;
+
+            return portal;
+        }
+
+        return null;
+    }
+
+
+
 }
