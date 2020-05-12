2111098f462fa76ccbd51ca80f8be1e5d775aa2f
MarkTemby <temby644@gmail.com>
2020-05-12 11:58:06 +0930

    Lesson 70

---
+++ Assets/Portal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+
+    //** variables
+    [SerializeField] int sceneToLoad = -1;
+
+    //** Start and Update
+    void Start()
+    {
+       
+    }
+        
+    //** Public methods
+       
+    //** Private methods
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
