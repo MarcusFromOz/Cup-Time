f16dd6cbc617564754a761fec26942db22396477
MarkTemby <temby644@gmail.com>
2020-05-14 07:21:15 +0930

    UI - first steps

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    [SerializeField] float speedHorizontal = 2.0f;
+    [SerializeField] float speedVertical = 2.0f;
+
+    float yaw = 0.0f;
+    float pitch = 0.0f;
+
+
+    void Update()
+    {
+
+        if (Input.GetMouseButton(1))
+        {
+
+            if (Input.GetMouseButton(0)) return;
+            
+            yaw += speedHorizontal * Input.GetAxis("Mouse X");
+            pitch -= speedVertical * Input.GetAxis("Mouse Y");
+
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        }
+    }
+
+    
+
+
+}
