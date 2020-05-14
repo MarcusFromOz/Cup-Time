ed6cf61b1cbe3d1617843f787fa265119b4f630b
MarkTemby <temby644@gmail.com>
2020-05-14 16:27:08 +0930

    Got the trophy collection to show on UI - wohoo

--- Assets/Scripts/Trophy.cs
+++ Assets/Scripts/Trophy.cs
@@ -2,45 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Core;
+using TMPro;
 
 public class Trophy : MonoBehaviour
 {
     Health player;
     [SerializeField] float healAmount = 50f;
+    int numberOfTrophies;
+    public GameObject[] years;
+    private TextMeshProUGUI textMeshProUGUI;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Get Trophies.ChildCount and populate this variable for the level
+        // use for the win condition
+    
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 
     private void OnTriggerEnter(Collider other)
     {
         
-        Debug.Log("Collided with trophy");
-
         player = other.GetComponent<Health>();
 
-        // Add 50ish to health
+        
         if (other.tag == "Player")
-        { 
-            player.HealDamage(healAmount); 
+        {
+            // Add 50ish to players health
+            player.HealDamage(healAmount);
+            
+            Debug.Log(gameObject.tag + " was collided with");
+
+            //ToDo - remove hardcoded 1977 
+            if (gameObject.tag != null)
+            {
+                SetTrophyAsCollected();
+            }
+
+            Destroy(gameObject);
+
         }
 
-
         //Add to UI
+        // Load up list of trophies in the UI - 
+
 
         // Make the trophy disappear (with effects - over 2 seconds)
-
         // Check the win condition
-
         // throw Update some info to the canvas
     }
 
+    private void SetTrophyAsCollected()
+    {
+        years = GameObject.FindGameObjectsWithTag(gameObject.tag);
+
+        foreach (GameObject year in years)
+        {
+            textMeshProUGUI = year.GetComponent<TextMeshProUGUI>();
+            if (textMeshProUGUI != null)
+            {
+                textMeshProUGUI.color = Color.white;
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/Years.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Years : MonoBehaviour
+{
+    //private TextMeshPro textMeshPro;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //textMeshPro = GetComponent<TextMeshPro>();
+    }
+
+
+    //public void SetYearAsCollected(string year)
+    //{
+    //    foreach (TextMeshPro textMeshPro in FindObjectsOfType<TextMeshPro>())
+    //    {
+    //        if (textMeshPro.name == year) continue;
+    //        {
+    //            textMeshPro.color = Color.white;
+    //        }
+            
+    //    }
+    //}
+       
+}
