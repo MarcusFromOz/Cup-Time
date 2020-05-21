063298d995ffc774e11453fadeee739f44f4474e
MarkTemby <temby644@gmail.com>
2020-05-21 11:06:37 +0930

    Lesson 136

--- Assets/Scripts/V_Stats/BaseStats.cs
+++ Assets/Scripts/V_Stats/BaseStats.cs
@@ -5,19 +5,14 @@ namespace RPG.Stats
     public class BaseStats : MonoBehaviour
     {
         [Range(1, 99)] [SerializeField] int startingLevel = 1;
+
         [SerializeField] CharacterClass characterClass;
         [SerializeField] Progression progression = null;
 
-        public float GetHealth()
+        public float GetStat(Stat stat)
         {
-            return progression.GetHealth(characterClass, startingLevel);
+            return progression.GetStat(stat, characterClass, startingLevel);
         }
-
-        public float GetExperienceReward()
-        {
-            return 10;
-        }
-
     }
 }
 
--- Assets/Scripts/V_Stats/Progression.cs
+++ Assets/Scripts/V_Stats/Progression.cs
@@ -3,29 +3,44 @@
 namespace RPG.Stats
 {
     [CreateAssetMenu(fileName = "Progression", menuName = "Stats/New Progression", order = 0)]
-    public class Progression : ScriptableObject 
+    public class Progression : ScriptableObject
     {
         [SerializeField] ProgressionCharacterClass[] characterClasses = null;
 
-        public float GetHealth(CharacterClass characterClass, int level)
+
+        public float GetStat(Stat stat, CharacterClass characterClass, int level)
         {
             foreach (ProgressionCharacterClass progressionClass in characterClasses)
             {
-                if (progressionClass.characterClass == characterClass)
+                if (progressionClass.characterClass != characterClass) continue;
+
+                foreach (ProgressionStat progressionStat in progressionClass.stats)
                 {
-                    return progressionClass.health[level - 1];
+                    if (progressionStat.stat != stat) continue;
+
+                    if (progressionStat.levels.Length < level) continue;
+
+                    return progressionStat.levels[level - 1];
                 }
             }
-            return 30;
+            return 0;
         }
+
 
         [System.Serializable]
         class ProgressionCharacterClass
         {
             public CharacterClass characterClass;
-            public float[] health;
-            public float[] damage;
+            public ProgressionStat[] stats;
         }
+
+        [System.Serializable]
+        class ProgressionStat
+        {
+        public Stat stat;
+        public float[] levels;
+        }
+
     }
 }
 
