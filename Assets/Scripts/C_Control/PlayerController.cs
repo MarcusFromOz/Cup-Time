63c4551ab8ef80eb7ec80b5e520d37b94259a6e7
MarkTemby <temby644@gmail.com>
2020-05-23 11:01:06 +0930

    Lesson 153 - basic cursors

--- Assets/Scripts/C_Control/PlayerController.cs
+++ Assets/Scripts/C_Control/PlayerController.cs
@@ -2,15 +2,31 @@
 using RPG.Movement;
 using RPG.Combat;
 using RPG.Resources;
+using System;
 
 namespace RPG.Control
 {
     public class PlayerController : MonoBehaviour
     {
-        //** variables
         Health health;
 
-        //** start and update
+        enum CursorType
+        {
+            None,
+            Movement,
+            Combat
+        }
+
+        [System.Serializable]
+        struct CursorMapping
+        {
+            public CursorType type;
+            public Texture2D texture;
+            public Vector2 hotspot;
+        }
+
+        [SerializeField] CursorMapping[] cursorMappings = null;
+
         private void Awake()
         {
             health = GetComponent<Health>();
@@ -24,14 +40,12 @@ namespace RPG.Control
             if (InteractWithCombat()) return; 
             
             if (InteractWithMovement()) return;
+
+            SetCursor(CursorType.None);
+
         }
-
-        //** public functions
-
-
-
-        //** private functions
         
+
         private bool InteractWithCombat()
         {
             RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
@@ -47,10 +61,30 @@ namespace RPG.Control
                     {
                         GetComponent<Fighter>().Attack(target.gameObject);
                     }
-                    return true;
+                SetCursor(CursorType.Combat);
+                return true;
             }
             return false;
         }
+
+        private void SetCursor(CursorType type)
+        {
+            CursorMapping mapping = GetCursorMapping(type);
+            Cursor.SetCursor(mapping.texture, mapping.hotspot, CursorMode.Auto);
+        }
+
+        private CursorMapping GetCursorMapping(CursorType type)
+        {
+            foreach (CursorMapping mapping in cursorMappings)
+            {
+                if (mapping.type == type)
+                {
+                    return mapping;
+                }
+            }
+            return cursorMappings[0];
+        }
+
 
         private bool InteractWithMovement()
         {
@@ -64,6 +98,7 @@ namespace RPG.Control
                 {  
                     GetComponent<Mover>().StartMoveAction(hit.point, 1f); 
                 }
+                SetCursor(CursorType.Movement);
                 return true;
             }
             return false;
