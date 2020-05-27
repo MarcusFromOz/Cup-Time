7d1122deb1d5f6cbb9dfbd7a21a0703c18dd4d23
MarkTemby <temby644@gmail.com>
2020-05-27 22:46:07 +0930

    Did more with cinemamachine

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraScript : MonoBehaviour
 {
 
     [SerializeField] float speedHorizontal = 2.0f;
     [SerializeField] float speedVertical = 2.0f;
+    [SerializeField] float zoomAmount = 5.0f;
 
     float yaw = 0.0f;
     float pitch = 0.0f;
-
-
+    CinemachineVirtualCamera virtualCamera;
+    
     void Update()
     {
 
@@ -25,6 +27,33 @@ public class CameraScript : MonoBehaviour
 
             transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
         }
+
+        if (Input.GetMouseButton(2))
+        {
+            //want to zoom in and out with mousewheel
+            //print("***middle mouse button down***");
+
+        }
+
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            //print("***zoom out***");
+            //virtualCamera.m_Lens.FieldOfView += zoomAmount;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            //print("***zoom in***");
+            //virtualCamera.m_Lens.FieldOfView -= zoomAmount;
+        }
+        
+        //clamp it?
+
+
+       
+
+
     }
 
     
