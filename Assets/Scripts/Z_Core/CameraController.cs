b80366f775597c0aac6a86bf1e9b4f1fd3424962
MarkTemby <temby644@gmail.com>
2020-06-06 07:04:12 +0930

    Added the freelook camera and some ingame help

---
+++ Assets/Scripts/Z_Core/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using Cinemachine;
+using RPG.Control;
+
+namespace RPG.Core
+{
+
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] GameObject freeLookCamera;
+        CinemachineFreeLook freeLookComponent;
+        PlayerController playerControllerScript;
+
+        // Start is called before the first frame update
+        private void Awake()
+        {
+            freeLookComponent = freeLookCamera.GetComponent<CinemachineFreeLook>();
+            playerControllerScript = GetComponent<PlayerController>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                //if (playerControllerScript.isDraggingUI) return;
+
+                freeLookComponent.m_XAxis.m_MaxSpeed = 500;
+            }
+
+            if (Input.GetMouseButtonUp(1))
+            {
+                freeLookComponent.m_XAxis.m_MaxSpeed = 0;
+            }
+
+            if (Input.mouseScrollDelta.y != 0)
+            {
+                freeLookComponent.m_YAxis.m_MaxSpeed = 10;
+            }
+
+        }
+    }
+}
+
--- Assets/Scripts/Z_Core/CameraFacing.cs
+++ Assets/Scripts/Z_Core/CameraFacing.cs
@@ -1,12 +1,20 @@
 ﻿using UnityEngine;
+using Cinemachine;
 
 namespace RPG.Core
 {
     public class CameraFacing : MonoBehaviour
     {
+        [SerializeField] CinemachineFreeLook playerFramingCamera;
+
+        private void Start()
+        {
+            playerFramingCamera = GameObject.FindGameObjectWithTag("PlayerFramingCamera").GetComponent<CinemachineFreeLook>();
+        }
         void LateUpdate()
         {
-            transform.forward = Camera.main.transform.forward;
+            transform.LookAt(2 * transform.position - playerFramingCamera.transform.position);
+            //transform.forward = Camera.main.transform.forward;
         }
     }
 }
